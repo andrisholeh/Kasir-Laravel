fad113645b2f57b5c22f9b9fb7c44974e324d0ea
Andri Sholeh <andisholeh.hafizh@gmail.com>
2025-12-05 13:27:57 +0700

    Routes

--- pos-laravel/routes/web.php
+++ pos-laravel/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\StokMasukController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\DetailPenjualanController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
